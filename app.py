ce6ead9edc98b4e96017d321fab439abe0ce0b72
Robert Tsui <rt.roberttsui@gmail.com>
2026-01-02 13:40:30 -0800

    created a db persistence layer and implemented methods for connection and querying the db

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, g 
+from todos.database_persistence.py import DatabasePersistence
 
 app = Flask(__name__)
 
+@app.before_request
+def load_storage():
+    g.storage = DatabasePersistence()
+    
 @app.route("/")
 def index():
     return "<h1>This is a todo starter!</h1>"
---
+++ todos/database_persistence.py
@@ -0,0 +1,31 @@
+class DatabasePersistence:
+    def __init__(self):
+        pass
+
+    def _database_connect(self):
+        pass 
+
+    def all_lists(self):
+        pass
+    
+    def create_list(self):
+        pass 
+
+    def view_list(self):
+        pass 
+
+    def edit_list(self):
+        pass 
+
+    def delete_list(self):
+        pass 
+
+    def create_todo(self):
+        pass 
+
+    def update_todo_status(self):
+        pass 
+
+    def complete_all_todos(self):
+        pass 
+
